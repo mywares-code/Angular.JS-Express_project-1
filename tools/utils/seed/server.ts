mgechev/angular-seed

commit 215bdbf610cd3c11624de3e5b0132660677d03bd

Fix spelling error Remove duplicates

tools/utils/seed/server.ts
@@ -8,6 +8,7 @@ import Config from '../../config';
 import * as codeChangeTool from './code_change_tools';
 
 const plugins = <any>gulpLoadPlugins();
+
 /**
  * Serves the Single Page Application. More specifically, calls the `listen` method, which itself launches BrowserSync.
  */
@@ -52,6 +53,7 @@ export function watchAppFiles(path: string, fileChangeCallback: (e: any, done: (
     fileChangeCallback(e, afterCall);
   });
 }
+
 /**
  * Starts a new `express` server, serving the static documentation files.
  */
@@ -64,7 +66,7 @@ export function serveDocs() {
   );
 
   server.listen(Config.DOCS_PORT, () =>
-    openResource('http://localhost:' + Config.DOCS_PORT + Config.APP_BASE)
+    openResource(getResourceUrl())
   );
 }
 
@@ -80,7 +82,7 @@ export function serveCoverage() {
   );
 
   server.listen(Config.COVERAGE_PORT, () =>
-    openResource('http://localhost:' + Config.COVERAGE_PORT + Config.APP_BASE)
+    openResource(getResourceUrl())
   );
 }
 
@@ -100,6 +102,10 @@ export function serveProd() {
   server.use(fallback('index.html', { root }));
 
   server.listen(Config.PORT, () =>
-    openResource('http://localhost:' + Config.PORT + Config.APP_BASE)
+    openResource(getResourceUrl())
   );
+}
+
+function getResourceUrl() {
+  return `http://localhost:{Config.PORT}{Config.APP_BASE}`;
 }

tools/tasks/seed/build.html_css.ts
@@ -36,7 +36,7 @@ if (isProd) {
 
 const appSCSSFiles      = join(Config.APP_SRC, '**', '*.scss');
 const entrySCSSFiles    = join(Config.CSS_SRC, '**', '*.scss');
-const abtractSCSSFiles  = join(Config.SCSS_SRC, '**', '*.scss');
+const abstractSCSSFiles = join(Config.SCSS_SRC, '**', '*.scss');
 
 /**
  * Copies all HTML files in `src/client` over to the `dist/tmp` directory.
@@ -62,7 +62,7 @@ function processComponentStylesheets() {
  * Process scss files referenced from Angular component `styleUrls` metadata
  */
 function processComponentScss() {
-  return getSCSSFiles('process-component-scss', [appSCSSFiles], [abtractSCSSFiles])
+  return getSCSSFiles('process-component-scss', [appSCSSFiles], [abstractSCSSFiles])
     .pipe(plugins.sourcemaps.init())
     .pipe(plugins.sass(Config.getPluginConfig('gulp-sass')).on('error', plugins.sass.logError))
     .pipe(plugins.postcss(processors))
@@ -143,7 +143,7 @@ function getExternalCss() {
  * Get a stream of external scss files for subsequent processing.
  */
 function getExternalScssStream() {
-  return getSCSSFiles('process-external-scss', getExternalScss(), [abtractSCSSFiles])
+  return getSCSSFiles('process-external-scss', getExternalScss(), [abstractSCSSFiles])
     .pipe(plugins.sass(Config.getPluginConfig('gulp-sass')).on('error', plugins.sass.logError));
 }
 

mywares-code/Angular.JS-Express_project-1

commit 04042f4cefd15b8867524b7f7a0f47ebed593ef5

Fix web browser not opening on serve tasks
Bug introduced in 215bdbf610cd3c11624de3e5b0132660677d03bd
Also revert using the same port for serving docs, coverage and prod.

tools/utils/seed/server.ts
@@ -66,7 +66,7 @@ export function serveDocs() {
   );
 
   server.listen(Config.DOCS_PORT, () =>
-    openResource(getResourceUrl())
+    openResource(getResourceUrl(Config.DOCS_PORT))
   );
 }
 
@@ -82,7 +82,7 @@ export function serveCoverage() {
   );
 
   server.listen(Config.COVERAGE_PORT, () =>
-    openResource(getResourceUrl())
+    openResource(getResourceUrl(Config.COVERAGE_PORT))
   );
 }
 
@@ -102,10 +102,10 @@ export function serveProd() {
   server.use(fallback('index.html', { root }));
 
   server.listen(Config.PORT, () =>
-    openResource(getResourceUrl())
+    openResource(getResourceUrl(Config.PORT))
   );
 }
 
-function getResourceUrl() {
-  return `http://localhost:{Config.PORT}{Config.APP_BASE}`;
+function getResourceUrl(port: number) {
+  return `http://localhost:${port}${Config.APP_BASE}`;
 }